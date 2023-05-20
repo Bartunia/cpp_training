8b992d049f4d0e202080d692b44d1ca5faba1e6c
Bartunia <wierzchos.bartlomiej@gmail.com>
2023-05-20 12:55:45 +0200

    first commit

---
+++ Program_01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Enter your favorite number between 1 and 100: ";
+    int number;
+    cin >> number;
+    cout << "Wow! " << number << " is also my favorite number!";
+    return 0;
+}
